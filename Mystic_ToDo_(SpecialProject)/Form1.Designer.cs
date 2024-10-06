09bd0fd7e293c316029e65847df3be506c48d8ab
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-10-06 10:57:30 -0500

    Project Creation

--- Mystic_ToDo_(SpecialProject)/Form1.Designer.cs
+++ Mystic_ToDo_(SpecialProject)/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mystic_ToDo__SpecialProject_
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1898, 1024);
+            Name = "MainWindow";
+            Text = "Mystic ToDo";
+            ResumeLayout(false);
         }
 
         #endregion
